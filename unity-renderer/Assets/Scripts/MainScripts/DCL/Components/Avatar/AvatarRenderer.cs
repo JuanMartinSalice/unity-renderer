0b54aba56084cd8635451e97f2d30c37085dab18
Brian Amadori <black.pentagram@gmail.com>
2021-08-25 22:46:29 -0300

    remove face hack

--- unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarRenderer.cs
+++ unity-renderer/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarRenderer.cs
@@ -681,14 +681,6 @@ namespace DCL
                 avatarMeshCombiner.container.transform.localPosition = Vector3.zero;
             }
 
-            var renderersToHide = allRenderers.Where((r) => r.transform.parent.gameObject.name.Contains("Mask")).ToList();
-
-            foreach ( var r in renderersToHide )
-            {
-                r.forceRenderingOff = true;
-                r.gameObject.SetActive(false);
-            }
-
             return success;
         }
 
